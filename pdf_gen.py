c8f3077c8ad15fc34117a6f5dedf27a5dc216efe
Dav Vrat Chadha <95514650+DavVratChadha@users.noreply.github.com>
2023-01-01 12:08:18 -0500

    Add files via upload

---
+++ pdf_gen.py
@@ -0,0 +1,18 @@
+#This program assumes all the images have been named as follows: page<number><.extension>
+#Importing modules
+from PIL import Image
+
+#Enter start and End number of images
+start = 1
+end = 2
+#Insert image type extension here:
+im_type = ".png"
+#Insert the directory here
+dir = ""
+filename = "file.pdf"
+
+im_list = []
+for i in range(start, end + 1):
+    image = dir + "/page" + str(i) + im_type
+    im_list.append(Image.open(image).convert("RGB"))
+im_list[0].save(dir + "/" + filename, save_all = True, append_images = im_list)
